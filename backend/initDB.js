e5e032d9fe4aa86c7d5cbe5859ab70c989a370ce
lacyelln <lacyellenmiller@gmail.com>
2025-11-14 13:40:31 -0700

    installed openai and changed id to userId

--- backend/initDB.js
+++ backend/initDB.js
@@ -4,7 +4,7 @@ import db from "./db.js";
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      userId INTEGER PRIMARY KEY AUTOINCREMENT,
       userName TEXT NOT NULL,
       roomCode TEXT NOT NULL
     )
@@ -12,7 +12,7 @@ db.serialize(() => {
 
   db.run(`
     CREATE TABLE IF NOT EXISTS survey (
-      id INTEGER NOT NULL PRIMARY KEY,
+      userId INTEGER NOT NULL PRIMARY KEY,
       userName TEXT NOT NULL,
       data TEXT NOT NULL
     )
