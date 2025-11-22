c25d2e67ba2f8e1ae11b64d373b6b65b99a55ad8
lacyelln <lacyellenmiller@gmail.com>
2025-11-21 23:22:24 -0700

    created a gameOne file and what I could remember is needed

---
+++ my-app/src/survey/gameOne.jsx
@@ -0,0 +1,11 @@
+export function gameOne(){
+
+    //description of game
+
+    //roles that can be randomly asigned to user in the backend and then displayed in llm page
+
+    //survey questions
+
+}
+
+export default gameOne;
