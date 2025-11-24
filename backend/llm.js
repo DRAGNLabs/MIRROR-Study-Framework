e14d3b187a467b155697b13f7171de5fe77e37b0
tesessa <tessaeliseandersen@yahoo.com>
2025-11-24 14:15:34 -0700

    cleaning up code again

--- backend/llm.js
+++ backend/llm.js
@@ -16,34 +16,3 @@ export async function streamLLM(prompt, onToken) {
     }
   }
 }
-
-
-
-
-// import { openai } from '@ai-sdk/openai';
-// import { streamText } from 'ai';
-// import 'dotenv/config';
-
-
-// export async function respondToUser(userPrompts) {
-//   // Construct the LLM prompt
-//   const prompt = ``;
-
-//   // Send prompt to LLM
-//   const result = await streamText({
-//     model: openai('gpt-4.1-nano'),
-//     prompt: prompt.trim()
-//   });
-
-//   // Collect the streamed output
-//   let fullResponse = '';
-//   for await (const delta of result.textStream) {
-//     fullResponse += delta;
-//   }
-//   return fullResponse;
-
-// }
-
-//idk what form to make this return
-
-
