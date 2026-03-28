b6d1a355f93c274e1cfb34b2853cf89e68774252
tesessa <tessaeliseandersen@yahoo.com>
2026-03-27 21:43:13 -0600

    chaning err message

--- my-app/src/components/waiting/AdminWaitingRoom.jsx
+++ my-app/src/components/waiting/AdminWaitingRoom.jsx
@@ -50,7 +50,7 @@ export default function RoomManagement() {
       try {
         await setRole(user.userId, roleToAssign.id);
       } catch (error) {
-        console.log(error.message);
+        console.err(error.message);
       }
     }
   }
